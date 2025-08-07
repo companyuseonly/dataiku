245a0c39ab3424475f7acacb9b1047bbff06a5ad
Balamurugan P M <balamuruganedsty@gmail.com>
2025-08-07 21:36:36 +0530

    v1.2.1

---
+++ product/index.php
@@ -0,0 +1,65 @@
+<?php
+    $folderPath = "../";
+    $pageTitle = "Dataiku Product | The Universal AI Platform™ ";
+    $pageMeta = "";
+    $pageDescription = "";
+    $thisPage = "Dataiku Product | The Universal AI Platform™ ";
+    
+    include($folderPath . "Components/header.php");
+?>
+
+ <section class="margin-top-160 mob8">
+
+                <div class="container updated mob8">
+                    <div class="six columns right40">
+                        <h1 class="top40 font90">
+                            Discover Dataiku for Enterprise AI </h1>
+                        <p class="top40 font25">
+                            Drive innovation by unifying all people (from business to tech) and all enterprise data work
+                            (from analytics to Generative AI) in one centralized, governed platform for Everyday AI.
+                        </p>
+                        <p class="top40 bot10">
+                            <a class="button border right24 mob8-top width215" href="<?php echo $folderPath ?>get-started/"
+                                target="_self">Start Your Dataiku Free Trial</a>
+
+                            <a class="button border revert mob8-top width215" href="<?php echo $folderPath ?>/home/contact-us/"
+                                target="_self">Contact Us</a>
+                        </p>
+                    </div>
+                    <div class="six columns top10 pad-mob80-top">
+
+
+
+                        <a class="lightboxlaunch" onclick="launchLightbox('H4A1vdGLttGXtUrEYENh15')">
+                            <div class="vidyard-video">
+                                <div data-bg="<?php echo $folderPath ?>assets/uploads/2023/10/discover-dataiku.png"
+                                    class="main-button-products-heading rocket-lazyload"
+                                    style="background-size: contain; auto;    background-repeat: no-repeat;">
+                                    <script>function launchLightbox(val) {
+                                            var players = VidyardV4.api.getPlayersByUUID(val);
+                                            var player = players[0];
+                                            player.showLightbox();
+                                        }
+                                    </script>
+                                    <svg width="87" height="87" viewbox="0 0 87 87" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path class="vidyard-circle" fill-rule="evenodd" clip-rule="evenodd"
+                                            d="M43.5 87C67.5244 87 87 67.5244 87 43.5C87 19.4756 67.5244 0 43.5 0C19.4756 0 0 19.4756 0 43.5C0 67.5244 19.4756 87 43.5 87Z"
+                                            fill="#221C35" fill-opacity="0.5" />
+
+                                        <path class="vidyard-icon" fill-rule="evenodd" clip-rule="evenodd"
+                                            d="M61.4841 43.396C62.2211 43.7646 62.2211 44.8164 61.4841 45.1849L33.0838 59.385C32.4189 59.7175 31.6366 59.234 31.6366 58.4906L31.6366 30.0903C31.6366 29.3469 32.4189 28.8634 33.0838 29.1959L61.4841 43.396Z"
+                                            fill="white" />
+                                    </svg>
+                                    <div id="product-video">
+                                        <img class="vidyard-player-embed" data-uuid="H4A1vdGLttGXtUrEYENh15" data-v="4"
+                                            data-type="lightbox" data-autoplay="1">
+                                    </div>
+                                </div>
+                        </a>
+
+                    </div>
+                </div>
+            </section>
+
+<?php include($folderPath . "Components/footer.php"); ?>
